521ca97991f3097c1728519172623d01d4cadc7f
puranmgr <puranmgr@gmail.com>
2019-04-29 21:21:29 +0100

    added profile and image js files

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
@@ -30,29 +32,9 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('Not found')
-            }
-        })
-        .catch(err => res.status(400).json('error getting user'))
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0]);
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
